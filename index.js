d46494204b7c0b8312aa558177064ea86fd7c4a1
akkhara <p.akkhara@gmail.com>
2025-05-01 19:22:49 +0700

    update map view

--- index.js
+++ index.js
@@ -73,10 +73,6 @@ app.use('/reports/usage', usageRoutes);
 const mapRoutes = require('./routes/map');
 app.use('/map', mapRoutes);
 
-// const mapRoutes = require('./routes/map');
-// // ใช้ Route สำหรับแผนที่
-// app.use('/map', mapRoutes);
-
 app.get('/', (req, res) => {
   if (!req.session.user) {
     return res.redirect('/login');

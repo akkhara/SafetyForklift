3a04d026f163f36c49aa20791afd617e8182f6e1
akkhara <p.akkhara@gmail.com>
2025-05-07 01:10:55 +0700

    Update import.js

--- routes/import.js
+++ routes/import.js
@@ -61,6 +61,7 @@ router.post('/', upload.single('importFile'), async (req, res, next) => {
     await client.query('BEGIN');
 
     const results = [];
+    console.log(rows[0]);
     for (const row of rows) {
       const name = row.ThaiName?.trim();
       const englishName = row.EnglishName?.trim();
